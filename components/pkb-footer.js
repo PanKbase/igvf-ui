2ef74508120094c6d6a063d7cc19c8b48d296fab
Parul-Kudtarkar <parulbioinf@gmail.com>
2025-01-05 09:26:55 +0000

    favicon

--- components/pkb-footer.js
+++ components/pkb-footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+
 function PkbFooter() {
   return (
     <div className="pkb-footer">
@@ -27,7 +28,6 @@ function PkbFooter() {
           </Link>
           <div className="submenu-footer">
             <Link href="/data-browser.html" className="submenu-item-footer">Data Browser</Link>
-            <Link href="/single-cell.html" className="submenu-item-footer">Integrated Cell Browser</Link>
             <Link href="http://tools.cmdga.org:3838/metadata_analysis/" className="submenu-item-footer">Donor Metadata</Link>
             <Link href="/apis.html" className="submenu-item-footer">APIs</Link>
           </div>
@@ -37,6 +37,7 @@ function PkbFooter() {
             Resources
           </Link>
           <div className="submenu-footer">
+            <Link href="/single-cell.html" className="submenu-item-footer">Integrated Cell Browser</Link>
             <Link href="/analytical-library.html" className="submenu-item-footer">Analytical Library</Link>
             <Link href="/publications.html" className="submenu-item-footer">Publications</Link>
           </div>
@@ -64,6 +65,17 @@ function PkbFooter() {
             <Link href="/tutorials.html" className="submenu-item-footer">Tutorials</Link>
             <Link href="/news.html" className="submenu-item-footer">News</Link>
           </div>
+        </div>
+      </div>
+      <div className="f-row" style={{ gap: '20px' }}>
+        <div className="logo">
+          <Link href="/">
+            <img style={{ height: '37px' }} src="https://hugeampkpncms.org/sites/default/files/users/user32/pankbase/PanKbase_logo-black.svg" alt="PanKbase Logo" />
+          </Link>
+        </div>
+        <div>
+          Supported by <strong>National Institutes of Health (NIH)</strong> grants <strong>U24 DK138515</strong>, <strong>U24 DK138512</strong><br />
+          Supplemental funds from the <strong>NIH Office of Data Science Strategies</strong>
         </div>
       </div>
     </div>
