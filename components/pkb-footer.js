3dae665dd53cee9b5ad9cfd0ec6597fb14a3df3c
Parul-Kudtarkar <parulbioinf@gmail.com>
2025-05-10 20:43:25 +0000

    footer

--- components/pkb-footer.js
+++ components/pkb-footer.js
@@ -1,77 +1,125 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+
+// Using the same pkbMenu structure from the header component
+export const pkbMenu = {
+    highlightItems: [
+        { label: "PanKgraph", path: "https://pankgraph.org/" },
+        { label: "Integrated Cell Browser", path: "/single-cell.html" },
+    ],
+    menuItems: [
+        {
+            label: "Data",
+            path: "",
+            subMenuItems: [
+                {
+                    label: "Donor Summary",
+                    path: "/donor-metadata.html",
+                },
+                { label: "Data Library", path: "https://data.pankbase.org" },
+                { label: "APIs", path: "/apis.html" },
+            ],
+        },
+        {
+            label: "Resources",
+            path: "",
+            subMenuItems: [
+                { label: "Integrated Cell Browser", path: "/single-cell.html" },
+                {
+                    label: "Analytical Library",
+                    path: "/analytical-library.html",
+                },
+                { label: "Publications", path: "/publications.html" },
+            ],
+        },
+        {
+            label: "About",
+            path: "",
+            subMenuItems: [
+                { label: "Project", path: "/projects.html" },
+                { label: "People", path: "/people.html" },
+                { label: "Policies", path: "/policies.html" },
+                { label: "Programs", path: "/programs.html" },
+                { label: "Collaborate", path: "/collaborate.html" },
+            ],
+        },
+        {
+            label: "Help",
+            path: "",
+            subMenuItems: [
+                { label: "Contact", path: "/contact.html" },
+                {
+                    label: "Metadata | Data Standards",
+                    path: "/metadata-data-standards.html",
+                },
+                { label: "Tools | Pipelines", path: "/tools-pipelines.html" },
+                { label: "Tutorials", path: "/tutorials.html" },
+                { label: "News", path: "/news.html" },
+            ],
+        },
+    ],
+};
 
 function PkbFooter() {
   return (
-      <div className="pkb-footer" style={{ display: 'flex', flexDirection: "column" }}>
-      <div className="menu-footer">
+    <div className="pkb-footer">
+      <div className="menu">
         <div className="main-menu-items">
-          <div className="menu-item-wrapper-footer">
-            <Link href="https://pankgraph.org/" className="menu-item menu-item-main">
-              PanKgraph
+          {pkbMenu.highlightItems.map((item, index) => (
+            <div key={`footer-highlight-${index}`} className="menu-item-wrapper">
+              <Link href={item.path} className="menu-item menu-item-main">
+                {item.label}
+              </Link>
+            </div>
+          ))}
+        </div>
+        
+        {pkbMenu.menuItems.map((item, index) => (
+          <div key={`footer-menu-${index}`} className="menu-item-wrapper">
+            <Link href={item.path || "#"} className="menu-item">
+              {item.label}
             </Link>
+            {item.subMenuItems && (
+              <div className="submenu">
+                {item.subMenuItems.map((subItem, subIndex) => (
+                  <Link
+                    key={`footer-submenu-${index}-${subIndex}`}
+                    href={subItem.path || "#"}
+                    className="submenu-item"
+                  >
+                    {subItem.label}
+                  </Link>
+                ))}
+              </div>
+            )}
           </div>
-          <div className="menu-item-wrapper-footer">
-            <Link href="https://pankbase.org/single-cell.html" className="menu-item menu-item-main">
-              Integrated Cell Browser
-            </Link>
-          </div>
-        </div>
-        <div className="menu-item-wrapper-footer">
-          <Link href="/" className="menu-item">
-            Data
-          </Link>
-          <div className="submenu-footer">
-            <Link href="https://pankbase.org/donor-metadata.html" className="submenu-item-footer">Donor Summary</Link>
-            <Link href="https://data.pankbase.org" className="submenu-item-footer">Data Library</Link>
-            <Link href="https://pankbase.org/apis.html" className="submenu-item-footer">APIs</Link>
-          </div>
-        </div>
-        <div className="menu-item-wrapper-footer">
-          <Link href="/" className="menu-item">
-            Resources
-          </Link>
-          <div className="submenu-footer">
-            <Link href="https://pankbase.org/single-cell.html" className="submenu-item-footer">Integrated Cell Browser</Link>
-            <Link href="https://pankbase.org/analytical-library.html" className="submenu-item-footer">Analytical Library</Link>
-            <Link href="https://pankbase.org/publications.html" className="submenu-item-footer">Publications</Link>
-          </div>
-        </div>
-        <div className="menu-item-wrapper-footer">
-          <Link href="/" className="menu-item">
-            About
-          </Link>
-          <div className="submenu-footer">
-            <Link href="https://pankbase.org/projects.html" className="submenu-item-footer">Project</Link>
-            <Link href="https://pankbase.org/people.html" className="submenu-item-footer">People</Link>
-            <Link href="https://pankbase.org/policies.html" className="submenu-item-footer">Policies</Link>
-            <Link href="https://pankbase.org/programs.html" className="submenu-item-footer">Programs</Link>
-            <Link href="https://pankbase.org/collaborate.html" className="submenu-item-footer">Collaborate</Link>
-          </div>
-        </div>
-        <div className="menu-item-wrapper-footer">
-          <Link href="/" className="menu-item">
-            Help
-          </Link>
-          <div className="submenu-footer">
-            <Link href="https://pankbase.org/contact.html" className="submenu-item-footer">Contact</Link>
-            <Link href="https://pankbase.org/metadata-data-standards.html" className="submenu-item-footer">Metadata | Data Standards</Link>
-            <Link href="https://pankbase.org/tools-pipelines.html" className="submenu-item-footer">Tools | Pipelines</Link>
-            <Link href="https://pankbase.org/tutorials.html" className="submenu-item-footer">Tutorials</Link>
-            <Link href="https://pankbase.org/news.html" className="submenu-item-footer">News</Link>
-          </div>
-        </div>
+        ))}
       </div>
+      
       <div className="f-row" style={{ gap: '20px' }}>
-        <div className="logo">
-          <Link href="/">
-            <img style={{ height: '37px' }} src="https://hugeampkpncms.org/sites/default/files/users/user32/pankbase/PanKbase_logo-black.svg" alt="PanKbase Logo" />
-          </Link>
-        </div>
+        <Link href="/">
+          <Image
+            width={37}
+            height={37}
+            style={{ height: '37px', width: 'auto' }}
+            src="https://hugeampkpncms.org/sites/default/files/users/user32/pankbase/PanKbase_logo-black.svg"
+            alt="PanKbase Logo"
+          />
+        </Link>
         <div>
           Supported by <strong>National Institutes of Health (NIH)</strong> grants <strong>U24 DK138515</strong>, <strong>U24 DK138512</strong><br />
           Supplemental funds from the <strong>NIH Office of Data Science Strategies</strong>
         </div>
+        <a href="https://hirnetwork.org/" target="_blank" rel="noopener noreferrer">
+          <Image
+            width={37}
+            height={37}
+            style={{ height: '37px', width: 'auto' }}
+            src="https://hugeampkpncms.org/sites/default/files/images/pankbase/logo-hirn.svg"
+            alt="HIRN Logo"
+          />
+        </a>
       </div>
     </div>
   );
