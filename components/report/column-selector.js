298cbac83c00815a9abfa08f7431efc54e888c22
Parul-Kudtarkar <parulbioinf@gmail.com>
2025-11-26 02:58:04 +0000

    visible column selector button

--- components/report/column-selector.js
+++ components/report/column-selector.js
@@ -104,7 +104,7 @@ export default function ColumnSelector({
 
     return (
       <>
-        <Button onClick={() => setIsOpen(true)} isDisabled={isDisabled}>
+        <Button type="secondary" onClick={() => setIsOpen(true)} isDisabled={isDisabled}>
           Columns
           <HiddenColumnsIndicator isAnyColumnHidden={isAnyColumnHidden} />
         </Button>
