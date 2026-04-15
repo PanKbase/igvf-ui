cba7a45131b1d89bfa872b167058b514d78edf24
Parul-Kudtarkar <parulbioinf@gmail.com>
2026-04-15 21:32:20 +0000

    login

--- components/session-context.js
+++ components/session-context.js
@@ -98,22 +98,22 @@ export function Session({ postLoginRedirectUri, children }) {
   // Get all the schemas so that the several other places in the code that need schemas can get
   // them from this context instead of doing a request to /profiles.
   useEffect(() => {
-    if (!profiles) {
-      getProfiles().then((response) => {
+    if (!profiles && dataProviderUrl) {
+      getProfiles(dataProviderUrl).then((response) => {
         setProfiles(response);
       });
     }
-  }, [profiles]);
+  }, [profiles, dataProviderUrl]);
 
   // Get the mapping of @type, collection name, and schema name to corresponding human-readable
   // names.
   useEffect(() => {
-    if (!collectionTitles) {
-      getCollectionTitles().then((response) => {
+    if (!collectionTitles && dataProviderUrl) {
+      getCollectionTitles(dataProviderUrl).then((response) => {
         setCollectionTitles(response);
       });
     }
-  }, [collectionTitles]);
+  }, [collectionTitles, dataProviderUrl]);
 
   // If we detect a transition from Auth0's logged-out state to logged-in state, log the user into
   // igvfd. The callback that auth0-react calls after a successful Auth0 login exists outside the
